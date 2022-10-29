a5b6d648a687c4c48c9396b02ed7c3cd06e14b71
alex0207s <alex640702070829@gmail.com>
2022-10-30 02:07:10 +0800

    :sparkles:feature: setup react-router && init project template

---
+++ src/pages/following/index.tsx
@@ -0,0 +1,11 @@
+const Following: React.FC = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">
+        Instagram Following page
+      </h1>
+    </div>
+  );
+};
+
+export default Following;
---
+++ src/pages/home/index.tsx
@@ -0,0 +1,9 @@
+const Home: React.FC = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <h1 className="text-3xl font-bold text-blue-600">Instagram Home page</h1>
+    </div>
+  );
+};
+
+export default Home;
