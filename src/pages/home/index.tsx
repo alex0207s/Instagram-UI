fd40f53ba5e18695cbe315019c35c9d800aa16de
alex0207s <alex640702070829@gmail.com>
2022-10-31 23:31:55 +0800

    :sparkles:feature: add header component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,8 +1,15 @@
+import Header from '../../components/Header';
+
 const Home: React.FC = () => {
   return (
-    <div className="min-h-screen flex justify-center items-center">
-      <h1 className="text-3xl font-bold text-blue-600">Instagram Home page</h1>
-    </div>
+    <>
+      <Header />
+      <div className="min-h-screen flex justify-center items-center">
+        <h1 className="text-3xl font-bold text-blue-600">
+          Instagram Home page
+        </h1>
+      </div>
+    </>
   );
 };
 
