a8970ac917389a12f632dd315bd6ba5d3ce649c0
alex0207s <alex640702070829@gmail.com>
2022-11-04 00:07:11 +0800

    :sparkles:feature: add user component

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,6 +1,7 @@
 import Header from '../../components/Header';
 import Container from '../../components/Container';
 import IGStory from './components/IGStory';
+import IGUser from '../../components/User';
 
 const Home: React.FC = () => {
   return (
@@ -11,7 +12,17 @@ const Home: React.FC = () => {
           <div className="w-full lg:w-[600px]">
             <IGStory />
           </div>
-          <div className="hidden lg:block lg:w-[424px]">IG Following</div>
+          <div className="hidden lg:block lg:w-[424px]">
+            <IGUser
+              size="medium"
+              showFollow
+              isFollowing
+              account="alex liao"
+              location="Taipei"
+              avatar="/images/avatars/a1.png"
+              id={0}
+            />
+          </div>
         </div>
       </Container>
       <div className="min-h-screen flex justify-center items-center">
