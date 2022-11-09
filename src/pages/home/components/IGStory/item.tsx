b944626f816105625e71bc5f31c2c157be7f833f
alex-c-liao <alex_c_liao@trendmicro.com>
2022-11-09 23:00:37 +0800

    :sparkles:feature: create && connect the mock instagram story API

--- src/pages/home/components/IGStory/item.tsx
+++ src/pages/home/components/IGStory/item.tsx
@@ -3,13 +3,26 @@ import React from 'react';
 type ItemProps = {
   name: string;
   avatar: string;
+  isLoading: boolean;
 };
 
 const Item: React.FC<React.PropsWithChildren<ItemProps>> = ({
   name,
   avatar,
+  isLoading,
 }) => {
-  return (
+  return isLoading ? (
+    <div className="text-center">
+      <div
+        className="w-[56px] h-[56px] border-2 bg-gray-200 border-gray-300 rounded-full mx-[11px] overflow-hidden"
+        style={{
+          backgroundPosition: 'center',
+          backgroundSize: 'cover',
+        }}
+      ></div>
+      <p className="text-xs mt-1"></p>
+    </div>
+  ) : (
     <div className="text-center">
       <div
         className="w-[56px] h-[56px] ring-2 border-2 border-white ring-red-500 rounded-full mx-[11px] overflow-hidden"
