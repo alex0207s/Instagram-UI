021287e5c833a86fac00607a5fe4cbfc3fc640f8
alex-c-liao <alex_c_liao@trendmicro.com>
2022-11-09 23:00:37 +0800

    :sparkles:feature: create && connect the mock instagram story API

--- src/pages/home/components/IGStory/index.tsx
+++ src/pages/home/components/IGStory/index.tsx
@@ -1,67 +1,24 @@
 import Item from './item';
+import { useGetIGStoriesQuery } from '../../../../services/homeService';
 
 const IGStory: React.FC = () => {
-  const mock_stories = [
-    {
-      id: 1,
-      name: 'bruce_fe',
-      avatar: '/images/avatars/a1.png',
-    },
-    {
-      id: 2,
-      name: 'max',
-      avatar: '/images/avatars/a2.png',
-    },
-    {
-      id: 3,
-      name: 'fm',
-      avatar: '/images/avatars/a3.png',
-    },
-    {
-      id: 4,
-      name: 'joanne',
-      avatar: '/images/avatars/a4.png',
-    },
-    {
-      id: 5,
-      name: 'focus',
-      avatar: '/images/avatars/a5.png',
-    },
-    {
-      id: 6,
-      name: 'louis',
-      avatar: '/images/avatars/a6.png',
-    },
-    {
-      id: 7,
-      name: 'alvin',
-      avatar: '/images/avatars/a7.png',
-    },
-    {
-      id: 8,
-      name: 'grace',
-      avatar: '/images/avatars/a8.png',
-    },
-    {
-      id: 9,
-      name: 'rance',
-      avatar: '/images/avatars/a9.png',
-    },
-    {
-      id: 10,
-      name: 'bruce_fe',
-      avatar: '/images/avatars/a10.png',
-    },
-  ];
-
-  mock_stories.map((item) => console.log(item));
+  const { data, isLoading } = useGetIGStoriesQuery('all');
 
   return (
     <div className="w-full h-[110px] box-border flex items-center overflow-x-auto overflow-y-hidden shadow-md no-scrollbar lg:my-8">
-      {mock_stories.map((item) => {
-        const { id, name, avatar } = item;
-        return <Item key={id} name={name} avatar={avatar} />;
-      })}
+      {isLoading &&
+        [1, 2, 3, 4, 5, 6, 7, 8].map((item) => {
+          return (
+            <Item key={item} isLoading={isLoading} name={''} avatar={''} />
+          );
+        })}
+      {!isLoading &&
+        data?.map((item) => {
+          const { id, name, avatar } = item;
+          return (
+            <Item key={id} isLoading={isLoading} name={name} avatar={avatar} />
+          );
+        })}
     </div>
   );
 };
