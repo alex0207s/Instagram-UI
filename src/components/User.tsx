54a7c824fbfb6d1231462f2e8715281a37c87e2c
alex-c-liao <alex_c_liao@trendmicro.com>
2022-11-11 23:28:34 +0800

    :zap:Use memo api to avoid redundant re-render

--- src/components/User.tsx
+++ src/components/User.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { useAppDispatch } from '../hooks';
 // action
 import { follow, unFollow } from '../slices/friendSlice';
+import { memo } from 'react';
 
 interface UserProps {
   size?: 'medium' | 'small';
@@ -14,54 +15,56 @@ interface UserProps {
   id?: number;
 }
 
-const IGUser: React.FC<React.PropsWithChildren<UserProps>> = ({
-  size = 'small',
-  showFollow = false,
-  isFollowing = false,
-  account,
-  location,
-  avatar,
-  id,
-}) => {
-  const dispatch = useAppDispatch();
+const IGUser: React.FC<React.PropsWithChildren<UserProps>> = memo(
+  ({
+    size = 'small',
+    showFollow = false,
+    isFollowing = false,
+    account,
+    location,
+    avatar,
+    id,
+  }) => {
+    const dispatch = useAppDispatch();
 
-  function followClickHandler() {
-    if (id === undefined) return;
-    if (isFollowing) {
-      dispatch(unFollow(id));
-    } else {
-      dispatch(follow(id));
+    function followClickHandler() {
+      if (id === undefined) return;
+      if (isFollowing) {
+        dispatch(unFollow(id));
+      } else {
+        dispatch(follow(id));
+      }
     }
-  }
 
-  return (
-    <div className="flex h-[70px] items-center box-border px-4">
-      <div
-        className={`${
-          size === 'small' ? 'w-[40px] h-[40px]' : 'w-[60px] h-[60px]'
-        } overflow-hidden rounded-full`}
-        style={{
-          backgroundImage: `url(${avatar})`,
-          backgroundPosition: 'center',
-          backgroundSize: 'cover',
-        }}
-      />
-      <div className="ml-4">
-        <p className="font-bold text-sm">{account}</p>
-        <p className="text-gray-400 text-xs">{location}</p>
-      </div>
-      {showFollow && (
-        <p
+    return (
+      <div className="flex h-[70px] items-center box-border px-4">
+        <div
           className={`${
-            isFollowing ? 'text-gray-700' : 'text-blue-400'
-          } ml-auto text-xs font-bold  cursor-pointer`}
-          onClick={followClickHandler}
-        >
-          {isFollowing ? 'Following' : 'Follow'}
-        </p>
-      )}
-    </div>
-  );
-};
+            size === 'small' ? 'w-[40px] h-[40px]' : 'w-[60px] h-[60px]'
+          } overflow-hidden rounded-full`}
+          style={{
+            backgroundImage: `url(${avatar})`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}
+        />
+        <div className="ml-4">
+          <p className="font-bold text-sm">{account}</p>
+          <p className="text-gray-400 text-xs">{location}</p>
+        </div>
+        {showFollow && (
+          <p
+            className={`${
+              isFollowing ? 'text-gray-700' : 'text-blue-400'
+            } ml-auto text-xs font-bold  cursor-pointer`}
+            onClick={followClickHandler}
+          >
+            {isFollowing ? 'Following' : 'Follow'}
+          </p>
+        )}
+      </div>
+    );
+  }
+);
 
 export default IGUser;
